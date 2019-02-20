0fe962429d2ce00ed9b0263bad38981be1b5db70
yizongk <yizongk@gmail.com>
2019-02-19 21:24:17 -0500

    added some codes to bindSocketk(). using socketaddr_ll from linux/if_packet.h file

--- src/socket.cpp
+++ src/socket.cpp
@@ -1,6 +1,7 @@
 #include <errno.h>
 #include <string>
 #include <unistd.h>
+#include <sys/socket.h>
 #include "socket.h"
 
 #define handle_error(msg) \
@@ -8,7 +9,9 @@
 
 /* Default constructor */
 Socket::Socket() {
-    
+    int fd = 0;
+    //std::string sock_path = "socket_fd";
+    int error_code = 0;
 }
 
 /* Default destructor */
@@ -32,14 +35,35 @@ bool Socket::openSocket() {
 
 /* Assigning a name to a socket 
  * In other word, giving the socket an address, and telling it the size (in bytes) of that specific address
+ * fd now has a physical file on file system somewhere
  * */
-/* bool Socket::bindSocket() {
-    if() {
+bool Socket::bindSocket(unsigned short sll_protocol_in, int sll_ifindex_in, unsigned char sll_halen_in, unsigned char* sll_addr_in, unsigned int sll_addr_in_len = 8) {
+    sockaddr_ll myaddr;
+    memset(&myaddr, 0, sizeof(sockaddr_ll));    //*memset(void *s, int c, size_t n)  -->  fills the first n bytes of the memory area pointed to by s with the constant byte c. Clears the structure
 
+    myaddr.sll_family = AF_PACKET;
+    myaddr.sll_protocol = ?;
+    myaddr.sll_ifindex = ?;
+    myaddr.sll_halen = ?;
+    for( int i = 0; i < sll_addr_in_len; ++i ) {
+        myaddr.sll_addr[i] = sll_addr_in[i];
+    }
+
+    if( bind( this->fd, (struct sockaddr *) &myaddr, sizeof(sockaddr_ll) ) != 0 ) {
+        error_code = errno;
+        handle_error("binding the socket");
+        return false;
     }
 
     return true;
-} */
+}
+
+/* delete a name (the fd that was binded) and possibly the file it refers to 
+ * in other word, unbinding it
+ * */
+bool Socket::unlinkeSocket() {
+
+}
 
 /* Release the resource associated with file descriptor of this socket */
 bool Socket::closeSocket() {
--- src/socket.h
+++ src/socket.h
@@ -2,18 +2,21 @@
 #define SOCKET_H
 
 #include <sys/socket.h>
+#include <linux/if_packet.h>
 #include <string.h>
 
 class Socket {
     private:
         int fd;
+        //std::string sock_path;
         int error_code;
     public:
         Socket();
         ~Socket();
 
         bool openSocket();
-        //bool bindSocket();
+        bool bindSocket(unsigned short sll_protocol_in, int sll_ifindex_in, unsigned char sll_halen_in, unsigned char* sll_addr_in, unsigned int sll_addr_in_len = 8);  // Before calling bindSocket, make sure ssl_addr_in is of size 8
+        bool unlinkeSocket();
         bool closeSocket();
 
 
