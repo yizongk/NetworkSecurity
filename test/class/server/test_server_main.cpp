15802ebc1a8a6014e0e153a2ea5334653c25baa3
yizongk <yizongk@gmail.com>
2019-03-03 19:46:05 -0500

    added test cases for server.h

--- test/class/server/test_server_main.cpp
+++ test/class/server/test_server_main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 int main(int argc, char* argv[]) {
 
     bool pass = true;
+    /* testing normal constructor and its functions */
     Server dummy;
 
     unsigned char *buf = new unsigned char[BUFLEN];
@@ -16,7 +17,7 @@ int main(int argc, char* argv[]) {
 
     ssize_t bytes = -1;
     unsigned char *incom_buf = new unsigned char[BUFLEN];
-    memset(buf,0,BUFLEN);
+    memset(incom_buf,0,BUFLEN);
 
     if( !dummy.bootup() ) {
         pass = false;
@@ -37,6 +38,36 @@ int main(int argc, char* argv[]) {
     if( !dummy.shutdown() ) {
         pass = false;
     }
+
+    printf("-------------------------------------------------------------------\n");
+    /* Testing constructor that takes in a interace name and its functions */
+    Server dummyItf("wlp59s0");
+    /* Reusing variable, but reset it first */
+    memset(buf,0,BUFLEN);
+    memcpy(buf,"Server test sending message: Second constructor NOW", 35);
+    bytes = -1;
+    memset(incom_buf,0,BUFLEN);
+
+    if( !dummyItf.bootup() ) {
+        pass = false;
+    }
+
+    if( !dummyItf.send(buf) ) {
+        pass = false;
+    } else {
+        cout << "Send out:'" << buf << "'" << endl;
+    }
+
+    if( !dummyItf.listen(incom_buf, bytes) ) {
+        pass = false;
+    } else {
+        cout << "Received(" << bytes << " bytes):'" << incom_buf << "'" << endl;
+    }
+
+    if( !dummyItf.shutdown() ) {
+        pass = false;
+    }
+
 
     delete buf;
     delete incom_buf;
