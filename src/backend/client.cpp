0fdd8c5dd51dbbe7e09f52b1a42d71d6fad5b2ea
yizongk <yizongk@gmail.com>
2019-03-05 15:57:29 -0500

    updated client to have private variable to store incoming packet's sockaddr infomration via linux API recvfrom()

--- src/backend/client.cpp
+++ src/backend/client.cpp
@@ -3,19 +3,20 @@
 
 /* Default constructor
  */
-Client::Client() : buffer_len(BUFF_MAX_LEN) {
-     
+Client::Client() : buffer_len(BUFF_MAX_LEN), incom_src_addr(), incom_src_addr_len(sizeof(incom_src_addr)) {
+     memset(&incom_src_addr,0,sizeof(incom_src_addr));
 }
 
 /*  Changes the default interface name that is used to bind the underlying socket
  */
-Client::Client(const char *interface) : endpoint(interface), buffer_len(BUFF_MAX_LEN) {
-
+Client::Client(const char *interface) : endpoint(interface), buffer_len(BUFF_MAX_LEN), incom_src_addr(), incom_src_addr_len(sizeof(incom_src_addr)) {
+    memset(&incom_src_addr,0,sizeof(incom_src_addr));
 }
 
 /* Ensure Client is closed.
  */
 Client::~Client() {
+    memset(&incom_src_addr,0,sizeof(incom_src_addr));
     this->shutdown();
 }
 
@@ -28,7 +29,7 @@ bool Client::bootup() {
 /* Listens for ONE incoming transmission. bytes is how much bytes was receieved.
  */
 bool Client::listen(unsigned char *buff, ssize_t &bytes) {
-    return endpoint.recvMsg(buff, this->buffer_len, bytes);
+    return endpoint.recvMsg(buff, this->buffer_len, bytes, this->incom_src_addr, this->incom_src_addr_len);
 }
 
 /* Sends ONE outgoing transmission.
@@ -43,6 +44,23 @@ bool Client::send(const unsigned char *buffer) {
  */
 bool Client::shutdown() {
     return endpoint.closeSocket();
+}
+
+/* 
+ * For the purpose of returning something of sockaddr that operator<< can process for std::cout
+ * In this case, we return a string of formated text
+ *  */
+std::string Client::get_string_src_addr() {
+    std::string format_output;
+    format_output = "Feature Coming Soon!";                 /* TODO */
+    return format_output;
+}
+
+/* 
+ * Returns the length of sockaddr len of the incoming packet
+ *  */
+int Client::get_src_addr_len() {
+    return this->incom_src_addr_len;
 }
 
 // Add fcts here to 
