73bca1a607923725b69e2f8ea3d05e2ca38ddabf
yizongk <yizongk@gmail.com>
2019-03-09 22:56:01 -0500

    renamed our protocol from defense to shinyarmor!

--- src/backend/shinyarmor_hdr.h
+++ src/backend/shinyarmor_hdr.h
@@ -2,14 +2,14 @@
  * Author: Yi Zong Kuang
  * This header is designed for packets passed through sockets
  * This header is intended to be appended after any TCP/IP header(if there is any), and before any payload
- * So any packets will look something like this [TCP/IP][defense_hdr][payload]
+ * So any packets will look something like this [TCP/IP][shinyarmor_hdr][payload]
  * 
  *  */
 
-#ifndef DEFENSE_HDR_H
-#define DEFENSE_HDR_H
+#ifndef SHINYARMOR_HDR_H
+#define SHINYARMOR_HDR_H
 
-struct defense_hdr {
+struct shinyarmor_hdr {
     bool eop = true;        // If true, this is the last packet of the entire transmission
 };
 
