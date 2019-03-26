5a700ec30501b3256ca4e1f88e8210bb84f0902c
KP1211 <42219424+KP1211@users.noreply.github.com>
2019-03-25 20:44:15 -0700

    protocol

--- src/backend/endpoint.cpp
+++ src/backend/endpoint.cpp
@@ -160,3 +160,15 @@ bool Endpoint::is_eof(unsigned char* income_buf) {
 }
 
 // Add fcts here to 
+
+void Endpoint::run_protocol_send(){
+      
+     
+
+}
+
+void Endpoint::run_protocol_rcv(){
+      
+     
+
+}
