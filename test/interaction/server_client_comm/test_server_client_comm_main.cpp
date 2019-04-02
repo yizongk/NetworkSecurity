9aaa8e7b94cd8f807a1b71b3de18c5d2bf72b2ed
yizongk <yizongk@gmail.com>
2019-04-02 15:52:35 -0400

    chore: Remove useless comments

--- test/interaction/server_client_comm/test_server_client_comm_main.cpp
+++ test/interaction/server_client_comm/test_server_client_comm_main.cpp
@@ -36,23 +36,11 @@ int main(int argc, char *argv[]) {
         memset(incom_buf,0,MAX_MSG_LEN);
         if( dummyServer.listen(incom_buf, bytes, PORT_NUM) ) {
 
-            /* printf("%d. Received(%zu):\n'",i,bytes);
-            for(int j=0;j<bytes;++j) {
-                printf("%02x ",incom_buf[j]);
-            }
-            printf("'\n"); */
-
             cout << i << ". Received(" << bytes << " bytes):" << endl << "'";
             for(int j = 0; j < bytes; ++j) {
                 cout << std::hex << (int)incom_buf[j];
             }
             cout << std::dec << "'" << endl;
-
-            /* struct ethhdr *eth = (struct ethhdr *)(incom_buf);
-            printf("\nEthernet Header\n");
-            printf("\t|-Source Address : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X\n",eth->h_source[0],eth->h_source[1],eth->h_source[2],eth->h_source[3],eth->h_source[4],eth->h_source[5]);
-            printf("\t|-Destination Address : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X\n",eth->h_dest[0],eth->h_dest[1],eth->h_dest[2],eth->h_dest[3],eth->h_dest[4],eth->h_dest[5]);
-            printf("\t|-Protocol : %d\n",eth->h_proto); */
         }
     }
     dummyServer.shutdown();
