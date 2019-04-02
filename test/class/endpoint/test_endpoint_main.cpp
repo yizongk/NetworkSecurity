1f12a7d3abf1a82bb5e3242198d94553734ff9af
yizongk <yizongk@gmail.com>
2019-04-02 15:12:51 -0400

    test: Update test cases for class Endpoint. Remove old test irrelavent test cases

--- test/class/endpoint/test_endpoint_main.cpp
+++ test/class/endpoint/test_endpoint_main.cpp
@@ -2,17 +2,14 @@
 #include "../../../src/backend/endpoint.h"
 
 #define BUFLEN 100
+#define PORTNUM 1000
 
 using namespace std;
-                                        //TODO: Need to fix output formating, and makefile!
-int main(int argc, char* argv[]) {
 
-    if( argc != 2 ) {
-        printf("Usage: sudo ./test_server_main 'interface name(such as lo, wlp59s0)'\n");
-        return 0;
-    }
+bool test_zero_arg_constructor() {
 
     bool pass = true;
+
     /* testing normal constructor and its functions */
     Endpoint dummy;
 
@@ -28,25 +25,41 @@ int main(int argc, char* argv[]) {
         pass = false;
     }
 
-    if( !dummy.send(buf) ) {
+    if( !dummy.send(buf, BUFLEN, PORTNUM) ) {
         pass = false;
     } else {
         cout << "Send out:'" << buf << "'" << endl;
     }
 
-    if( !dummy.listen(incom_buf, bytes) ) {
+    if( !dummy.listen(incom_buf, bytes, PORTNUM) ) {
         pass = false;
     } else {
         cout << "Received(" << bytes << " bytes):'" << incom_buf << "'" << endl;
-        cout << "\tPacket source addr size: '" << dummy.get_src_addr_len() << "'" << endl;
-        cout << "\tPacket sockaddr content: '" << dummy.get_string_src_addr() << "'" << endl;
     }
 
     if( !dummy.shutdown() ) {
         pass = false;
     }
 
-    printf("-------------------------------------------------------------------\n");
+    delete buf;
+    delete incom_buf;
+
+
+    return true;
+}
+
+bool test_cml_arg_constructor(char* argv[]) {
+
+    bool pass = true;
+ 
+    unsigned char *buf = new unsigned char[BUFLEN];
+    memset(buf,0,BUFLEN);
+    memcpy(buf,"Server test sending message: HAHAHA", 35);
+
+    ssize_t bytes = -1;
+    unsigned char *incom_buf = new unsigned char[BUFLEN];
+    memset(incom_buf,0,BUFLEN);
+
     /* Testing constructor that takes in a interace name and its functions */
     Endpoint dummyItf(argv[1]);
     /* Reusing variable, but reset it first */
@@ -59,18 +72,16 @@ int main(int argc, char* argv[]) {
         pass = false;
     }
 
-    if( !dummyItf.send(buf) ) {
+    if( !dummyItf.send(buf, BUFLEN, PORTNUM) ) {
         pass = false;
     } else {
         cout << "Send out:'" << buf << "'" << endl;
     }
 
-    if( !dummyItf.listen(incom_buf, bytes) ) {
+    if( !dummyItf.listen(incom_buf, bytes, PORTNUM) ) {
         pass = false;
     } else {
         cout << "Received(" << bytes << " bytes):'" << incom_buf << "'" << endl;
-        cout << "\tPacket source addr size: '" << dummy.get_src_addr_len() << "'" << endl;
-        cout << "\tPacket sockaddr content: '" << dummy.get_string_src_addr() << "'" << endl;
     }
 
     if( !dummyItf.shutdown() ) {
@@ -80,5 +91,33 @@ int main(int argc, char* argv[]) {
 
     delete buf;
     delete incom_buf;
+
+
+    return pass;
+}
+
+                                        //TODO: Need to fix output formating, and makefile!
+int main(int argc, char* argv[]) {
+
+    if( argc != 2 ) {
+        printf("Usage: sudo ./test_server_main 'interface name(such as lo, wlp59s0)'\n");
+        return 0;
+    }
+
+    bool pass = true;
+
+    cout << "-----------------PHASE 1 constructor with no argument-----------------" << endl;
+    if( !test_zero_arg_constructor() ) 
+        pass = false;
+
+    cout << "-----------------PHASE 2 constructor with command line argument-----------------" << endl;
+    if( !test_cml_arg_constructor(argv) )
+        pass = false; 
+    
+    if( pass ) 
+        cout << "\n---\n" << "all tests passed" << endl;
+    else 
+        cout << "\n---\n" << "some tests or all tests failed" << endl;
+
     return 0;
 }
